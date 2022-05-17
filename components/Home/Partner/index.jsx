bc82fc06abe80bd526452243ec128be5b0bd0129
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-05-17 20:42:04 +0700

    feat: add partnersip section

---
+++ components/Home/Partner/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import ImageSection from '../../ImageSection';
+import styles from './partner.module.css';
+
+const index = () => {
+  const data = [
+    {
+      src: '/static/images/acronis-logo.png',
+      alt: 'Acronis',
+      width: 400,
+      height: 50,
+    },
+    {
+      src: '/static/images/id-cloud-max-logo.png',
+      alt: 'Idcloudmax logo',
+      width: 400,
+      height: 60,
+    },
+    {
+      src: '/static/images/samgeo.png',
+      alt: 'Samgeo logo',
+      width: 400,
+      height: 60,
+    },
+
+    {
+      src: '/static/images/Tencent-Cloud-logo-compro.png',
+      alt: 'Tencent Cloud logo',
+      width: 400,
+      height: 60,
+    },
+  ];
+  return (
+    <section className={`partnership-image-list-section ${styles['image-list-section']}`}>
+      <div className='image-list-wrapper'>
+        <Container>
+          <h2 className={`text-center fw-bolder ${styles['title-color']}`}>
+            Our Partnership
+          </h2>
+          <ImageSection imageData={data} />
+        </Container>
+      </div>
+    </section>
+  );
+};
+
+export default index;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Layout from '../components/Layout';
 import Hero from '../components/Home/Hero';
 import Costumers from '../components/Home/Costumers';
 import Solutions from '../components/Home/Solutions';
+import Partner from '../components/Home/Partner';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Hero />
         <Costumers />
         <Solutions />
+        <Partner />
       </Layout>
     </>
   );
